136e30c048aee7db60855bfeabb1e77224bd3b3e
natashaval <natasha.valentina1998@gmail.com>
2024-02-09 15:12:53 +0000

    add trapezium and simpson DI

--- DependencyInjection/source/DependencyInjection.cpp
+++ DependencyInjection/source/DependencyInjection.cpp
@@ -12,12 +12,21 @@ int main()
         double x = delta_x * i;
         ys[i] = x*x;
     }
-    std::unique_ptr<Integrator> integratorPtr = std::make_unique<TrapeziumIntegrator>();
+    std::unique_ptr<Integrator> trapeziumIntegrator = std::make_unique<TrapeziumIntegrator>();
+
+    // DiscreteFunction DF(ys, 0, 1, std::make_unique<TrapeziumIntegrator>()); 
+    // NOT WORKING BECAUSE if you put it in the parameter, it doesn't have an address yet (temporary)
+    // so you should assign it first to the Left hand side (have an address), then it will pass by reference in the parameter
+
     // DiscreteFunction DF(ys, 0, 1, integratorPtr);
     DiscreteFunction DF(ys, 0, 1);
-    // DF.setIntegrator(integratorPtr);
+    DF.setIntegrator(trapeziumIntegrator);
 
-    std::cout << DF.integrate() << std::endl;
+    std::cout << "TrapeziumIntegrator: " << DF.integrate() << std::endl;
+
+    std::unique_ptr<Integrator> simpsonIntegrator = std::make_unique<SimpsonIntegrator>();
+    DF.setIntegrator(simpsonIntegrator);
+    std::cout << "SimpsonIntegrator: " << DF.integrate() << std::endl;
 
     return 0;
 }
